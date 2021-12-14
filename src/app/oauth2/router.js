c552ffee585190a9856e0b75b006390b5b281ff0
Robin Harrison <robin.harrison@digital.cabinet-office.gov.uk>
2021-12-14 13:33:06 +0000

    Add basic oauth flow code

---
+++ src/app/oauth2/router.js
@@ -0,0 +1,19 @@
+const express = require("express");
+
+const router = express.Router();
+
+const {
+  addAuthParamsToSession,
+  redirectToCallback,
+  redirectToDebugPage,
+  retrieveAuthorizationCode,
+} = require("./middleware");
+
+router.get(
+  "/authorize",
+  addAuthParamsToSession,
+  redirectToDebugPage
+);
+router.post("/authorize", retrieveAuthorizationCode, redirectToCallback);
+
+module.exports = router;
