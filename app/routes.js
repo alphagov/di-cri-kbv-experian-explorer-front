ff2ebddd6e2cb0892895e62f2d7151e6dbb6b427
Matthew Bidewell <matthewbidewell@gmail.com>
2022-01-11 10:44:22 +0000

    Call fraud API and print output

--- app/routes.js
+++ app/routes.js
@@ -2,62 +2,126 @@ const express = require('express')
 const axios = require('axios');
 const router = express.Router()
 
-const KBV_API_BASE_URL = process.env.KBV_API_BASE_URL
-const KBV_API_PATH = process.env.KBV_API_PATH
+const FRAUD_API_BASE_URL = "http://localhost:5007"
 
-router.post('/questions', function (req, res) {
-
+router.get('/', function (req, res) {
     const formValues = {
-        firstName: "albert",
-        surname: "arkil",
-        title: "mr",
-        dateOfBirth: "16/07/1948",
+        firstName: "",
+        surname: "",
+        dateOfBirth: "",
         addresses: [
             {
-                houseNameNumber: 3,
-                postcode: "CA14 5PH"
+                addressType: "",
+                street: "",
+                townCity: "",
+                houseNameNumber: 0,
+                postcode: ""
             }
         ]
     };
 
     // Make a request for a user with a given ID
-    axios.post(process.env.KBV_API_BASE_URL + '/saa', formValues)
+    axios.post(FRAUD_API_BASE_URL + '/identity-check', formValues)
         .then(function (response) {
-            if (response.data && response.data.questions && response.data.questions.question) {
+            res.status(200);
 
-                const response_data = {
-                    questions: response.data.questions.question,
-                    authRefNo: response.data.control.authRefNo,
-                    urn: response.data.control.urn,
-                };
+            const formattedResponse = {
+                headers: response.data.responseHeader,
+                orchestrationDecisions: response.data.clientResponsePayload.orchestrationDecisions,
+                decisionElements: response.data.clientResponsePayload.decisionElements
+            };
 
-                res.render('questions', response_data);
-            } else {
-                res.status(500);
-                res.render('error');
+            const orchestrationDecisions = orchestrationDecisionsToTable(
+                formattedResponse.orchestrationDecisions
+            );
+
+            const decisionElements = decisionElementsToTable(
+                formattedResponse.decisionElements
+            );
+
+            const formattedHeaders = summaryValuesToTable(
+                formattedResponse.headers
+            );
+
+            const formattedFormValues = summaryValuesToTable(
+                formValues
+            );
+
+            const outputTemplate = {
+                valuesSummaryList : formattedFormValues,
+                responseHeaderSummaryList: formattedHeaders,
+                responsePayloadOrchestrationDecisionTable: orchestrationDecisions,
+                responsePayloadDecisionElementsTable: decisionElements,
+                debugDataHtml: JSON.stringify(formattedResponse, undefined, 2)
             }
 
+            res.render('pretty-print', outputTemplate )
         })
-        .catch(function (error) {
+        .catch((error) => {
+            console.log(error.message)
             res.status(500);
             res.render('error');
         });
-
-
 });
 
+function orchestrationDecisionsToTable(orchestrationDecisions) {
+    const props = Object.keys(orchestrationDecisions[0]);
 
+    const head = props.map((prop) => {
+        return { text: prop };
+    });
 
-router.post('/answers', function (req, res) {
+    const rows = orchestrationDecisions.map((row) => {
+        return props.map((prop) => {
+            return {
+                text: row[prop],
+            };
+        });
+    });
 
+    return {
+        head,
+        rows,
+    };
+}
 
-    console.log("/answers req body")
-    console.log(JSON.stringify(req.body, null, 2))
-    console.log("..................................");
+function decisionElementsToTable(orchestrationDecisions){
+    const props = Object.keys(orchestrationDecisions[0]).filter((value) => {
+        return !["rules", "otherData", "matches", "dataCounts", "scores"].includes(
+            value
+        );
+    });
 
-    res.render('done');
+    const head = props.map((prop) => {
+        return { text: prop };
+    });
 
-});
+    const rows = orchestrationDecisions.map((row) => {
+        return props.map((prop) => {
+            return {
+                text: row[prop],
+            };
+        });
+    });
 
+    return {
+        head,
+        rows,
+    };
+}
+
+function summaryValuesToTable(keyValuePairs) {
+    const keyValues = [];
+    for (const [key, value] of Object.entries(keyValuePairs)) {
+        if(Array.isArray(value)) {
+            for(const val of value) {
+                keyValues.push(...summaryValuesToTable(val))
+            }
+        } else {
+            keyValues.push({key: {text: key}, value: {text: value}});
+        }
+    }
+    return keyValues;
+}
 
 module.exports = router
